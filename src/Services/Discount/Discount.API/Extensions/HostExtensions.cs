22a08b8d3dbdc04929af46bd0435656d6b35b48c
Jaroslav Šebok <jaro.sebok@gmail.com>
2021-08-19 16:52:12 +0200

    Discount.API dockerized, postgre migration unfinished

---
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+using System;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var configuration = services.GetRequiredService<IConfiguration>();
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+
+                try
+                {
+                    logger.LogInformation("Migrating postgresql configuration");
+
+                    using var connection = new NpgsqlConnection
+                        (configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+                    connection.Open();
+                    
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                return null; //TODO: finish migration
+            }
+        }
+    }
+}
