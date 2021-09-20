301c1652b645eb7ca7da3da6efbfae6b1bdf68cb
Jaroslav Šebok <jaro.sebok@gmail.com>
2021-09-20 15:50:01 +0200

    UpdateCommand, DeleteCommand, behaviors, service registration

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public ValidationException()
+            : base("One or more validation errors have occured.")
+        {
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> failures)
+        {
+            Errors = failures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());
+        }
+
+        public IDictionary<string, string[]> Errors { get; }
+    }
+}
